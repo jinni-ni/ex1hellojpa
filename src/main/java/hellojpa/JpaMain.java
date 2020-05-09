65226d66ad259906ca63c126b20b0078095bdf3a
김수진 <sjkim910115@gmail.com>
2020-05-09 14:04:04 +0900

    2020.05.09

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,7 +8,6 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.sql.SQLOutput;
 import java.util.List;
-
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -18,35 +17,20 @@ public class JpaMain {
         tx.begin();
 
         try{
-            // 비영속 상태
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
+            Member member = em.find(Member.class,150L);
+            member.setName("ZZZZ");
 
 
-            System.out.println("=====BEFORE=====");
-            // 영속 상태
-            // 객체를 저장한 상태
-            em.persist(member);
-            // 영속 상태 해제
-            //em.detach(member);
+//            Member member1 = new Member(150L,"A");
+//            Member member2 = new Member(160L,"B");
+//
+//
+//            em.persist(member1);
+//            em.persist(member2);
+//            System.out.println("======================");
 
-            // db 삭제
-            // em.remove(member);
-            System.out.println("=====AFTER=====");
-
-            // 1차 캐시에 저장되어 select 쿼리를 날리지 않음
-            em.find(Member.class,101L);
-            System.out.println("findMember.id = " + member.getId());
-            System.out.println("findMember.Name = " + member.getName());
-
-            // 쿼리를 날림
-            Member findMember2 = em.find(Member.class,100L);
-            // 1차 캐시에서 데이터 가져옴
-            Member findMember3 = em.find(Member.class,100L);
-
-
-            // 쿼리가 날아감
+            // commit 시에 sql 이 날아감
+            // flush : 영속성 컨텍스트의 변경사항과 database 를 맞춤
             tx.commit();
         } catch (Exception e){
             tx.rollback();
@@ -58,6 +42,36 @@ public class JpaMain {
     }
 
 }
+
+//public class JpaMain {
+//    public static void main(String[] args) {
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+//        EntityManager em = emf.createEntityManager();
+//
+//        EntityTransaction tx = em.getTransaction();
+//        tx.begin();
+//
+//        try{
+//            Member findMember1 = em.find(Member.class,101L);
+//            Member findMember2 = em.find(Member.class,101L);
+//
+//            //jpa가 영속 엔티티의 동일성을 보장
+//            //같은 트랜잭션 안에서 이루어 져야 함
+//            System.out.println("result = " + (findMember1 == findMember2));
+//
+//
+//            // 쿼리가 날아감
+//            tx.commit();
+//        } catch (Exception e){
+//            tx.rollback();
+//        }finally {
+//            em.close();
+//        }
+//
+//        emf.close();
+//    }
+//
+//}
 
 //public class JpaMain {
 //    public static void main(String[] args) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -16,6 +16,13 @@ public class Member {
     // @Column(name= "username")
     private String name;
 
+    public Member(){ }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
